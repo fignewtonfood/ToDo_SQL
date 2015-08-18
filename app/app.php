1e0b08950d509f34b1755be83e6310120ec1fd1d
Jordan Johansen & Tim White <student@epicodus.com>
2015-08-18 12:12:28 -0700

    debug twig and app.php files

--- app/app.php
+++ app/app.php
@@ -25,26 +25,29 @@
         return $app['twig']->render('index.html.twig', array('categories' => Category::getAll()));
     });
 
-    $app->get("/tasks", function() use ($app) {
+    // $app->get("/tasks", function() use ($app) {
+    //     var_dump(Task::find());
+    //     return $app['twig']->render('tasks.html.twig', array('tasks' => Task::find()));
+    //
+    // });
 
-        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
-
-    });
-
-    $app->get("/categories", function() use ($app) {
-        return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
-    });
+    // $app->get("/categories", function() use ($app) {
+    //     return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+    // });
 
     $app->get("/categories/{id}", function($id) use ($app){
         $category = Category::find($id);
         return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => $category->getTasks()));
-    })
+    });
 
     $app->post("/tasks", function() use ($app) {
 
-        $task = new Task($_POST['description']);
+        $description = $_POST['description'];
+        $category_id = $_POST['category_id'];
+        $task = new Task($description, $id=null, $category_id);
         $task->save();
-        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
+        $category = Category::find($category_id);
+        return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => $category->getTasks()));
     });
 
     $app->post("/categories", function() use ($app) {
@@ -55,6 +58,7 @@
 
     $app->post("/delete_categories", function() use ($app) {
         Category::deleteAll();
+        Task::deleteAll();
         return $app['twig']->render('index.html.twig');
     });
 
